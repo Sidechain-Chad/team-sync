ea290562e6f4d1290373e1d26b07cf6bb0b87fcb
Chad Niewenhuis <chadn94@gmail.com>
2026-01-17 21:38:06 +0200

    Defined backend board logic

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,9 @@
+class BoardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @board = Board.find(params[:id])
+    # The .includes avoids N+1 queries (A very common interview question!)
+    @lists = @board.lists.includes(:cards).order(:position)
+  end
+end
