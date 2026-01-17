6bcf08227a68d8b4a511793cfeb76e8c5027734e
Chad Niewenhuis <chadn94@gmail.com>
2026-01-17 21:25:08 +0200

    Defined models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,7 @@
 class Board < ApplicationRecord
   belongs_to :user
+  # Dependent and destroy will delete the card from the db
+  has_many :lists, -> { order(position: :asc) }, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,17 @@
 class List < ApplicationRecord
   belongs_to :board
+  # Dependent and destroy will delete the card from the db
+  has_many :card, -> { order(position: :asc) }, dependent: :destroy
+
+  validates :name, presence: true
+
+  # Before creating a new list, assign it to the last position
+  before_create :set_position
+
+  private
+  def set_position
+    # If there are no lists, make this #1. If there are, make this Last + 1.
+    last_position = board.lists.maximum(:position) || 0
+    self.position = last_position
+  end
 end
